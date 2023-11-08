d45644bfa1a50ac1239be65328a18126b7726de3
Gema <gema13bri@gmail.com>
2023-11-08 10:09:20 +0100

    Contactos activity

--- app/src/main/java/com/example/helloworld/ContactActivity.kt
+++ app/src/main/java/com/example/helloworld/ContactActivity.kt
@@ -2,19 +2,32 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.InputBinding
+import android.widget.Toast
 import com.example.helloworld.databinding.ActivityContactBinding
 
 class ContactActivity : AppCompatActivity() {
     private lateinit var binding: ActivityContactBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityContactBinding.inflate(layoutInflater)
+        //binding = ActivityContactBinding.inflate(layoutInflater) hacer invisible el email
         setContentView(binding.root)
 
        binding.switch1.setOnClickListener {
+           binding.email1.visibility = View.INVISIBLE
            binding.email1.isEnabled =
                binding.switch1.isChecked
        }
+        binding.btn.setOnClickListener{_ ->
+           when(binding.sexgroup.checkedRadioButtonId){
+               binding.male.id ->{
+                   Toast.makeText(this,"Sexo: masculino", Toast.LENGTH_SHORT).show()
+               }
+               binding.female.id ->{
+                   Toast.makeText(this,"Sexo: femenino", Toast.LENGTH_SHORT).show()
+               }
+           }
+        }
     }
 }
