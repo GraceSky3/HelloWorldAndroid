bafcf7b1fb95ff662c9c9884da7fdaafff04864a
Gema <gema13bri@gmail.com>
2023-11-10 10:23:36 +0100

    Username Activity

---
+++ app/src/main/java/com/example/helloworld/DisplayUserNameActivity.kt
@@ -0,0 +1,26 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.helloworld.databinding.ActivityDisplayUserNameBinding
+
+class DisplayUserNameActivity : AppCompatActivity() {
+
+    enum class Params{
+        USERNAME, PASSWORD
+    }
+
+    private lateinit var binding: ActivityDisplayUserNameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDisplayUserNameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = intent.getStringExtra(Params.USERNAME.name)
+
+        //cambiar el valor del la variable usernameText a la variable de la otra actividad
+        binding.usernametext.text= username
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/UserNameActivity.kt
@@ -0,0 +1,42 @@
+package com.example.helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.helloworld.databinding.ActivityUserNameBinding
+
+class UserNameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserNameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserNameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //boton para usuario
+        binding.opendisplayusernameactivity.setOnClickListener {
+            if (binding.username.text.isEmpty()){ //si esta vacio
+                Toast.makeText(this,"Debe introducir un nombre de usuario",
+                    Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent( //si ha metido el usario vamos a abrir la otra actividad
+                    this,  //ubicar la clase
+                    DisplayUserNameActivity::class.java
+                )
+                //el extra va a modificar el valor del username
+                intent.putExtra(
+                    DisplayUserNameActivity.Params.USERNAME.name,
+                    binding.username.text.toString()
+                )
+                startActivity(intent) //arranca la actividad
+
+            }
+
+        }
+    }
+}
+
+/*opendisplayusernameactivityIntent.putExtra(
+                    DisplayUserNameActivity.Params.USERNAME.name,
+                    binding.username.text.toString()
+                )*/
